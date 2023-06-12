e3a857f84cdfa8d1c7e9670b9b6009033d7dd0a9
EugeneLKL <eugenelkl02@gmail.com>
2023-06-12 23:51:52 +0800

    Nav bar design done

---
+++ src/NavBar.js
@@ -0,0 +1,15 @@
+
+
+const nav_bar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Hi-Bro</h1>
+            <div className="links">
+                <a style={{ width: '90px' }} href="/">Hiking</a>
+                <a style={{ width: '140px' }} href="/">Travel Buddy</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default nav_bar;
