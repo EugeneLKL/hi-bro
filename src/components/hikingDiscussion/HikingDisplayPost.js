3f071c1edc993ad2e36903b5b4ef78bbbecbb2a4
EugeneLKL <eugenelkl02@gmail.com>
2023-07-21 00:24:55 +0800

    functionality should be done

--- src/components/hikingDiscussion/HikingDisplayPost.js
+++ src/components/hikingDiscussion/HikingDisplayPost.js
@@ -1,5 +1,14 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import HikingVote from "../hikingPost/HikingVote";
+
+const discussionContainer = `
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-right: 10px;
+  margin-top: 10px;
+`;
 
 //TODO - Add username
 const HikingPost = ({ title, content, imageUrl, postId }) => {
@@ -21,45 +30,48 @@ const HikingPost = ({ title, content, imageUrl, postId }) => {
   };
 
   return (
-    <Link to={`hikingPost/${postId}`} className="post-link">
-      <div className="hiking-post">
-        <div className="hiking-post-header">
-          <img
-            className="profile-image"
-            src="/img/profileIcon.png"
-            alt="profile"
-          />
-          <h5 className="hiking-post-username">{username}</h5>
-        </div>
-        <h3 className="hiking-post-title">{title}</h3>
-        <div className="hiking-post-content">{content}</div>
-        {imageUrl && imageUrl.length > 0 && (
-          <div className="hiking-post-images">
-            <div className="image-slider">
-              <button
-                className="arrow-button prev-button"
-                onClick={handlePrevImage}
-                disabled={currentImageIndex === 0}
-              >
-                &lt;
-              </button>
-              <img
-                className="hiking-post-image"
-                src={imageUrl[currentImageIndex]}
-                alt={`Post ${currentImageIndex + 1}`}
-              />
-              <button
-                className="arrow-button next-button"
-                onClick={handleNextImage}
-                disabled={currentImageIndex === imageUrl.length - 1}
-              >
-                &gt;
-              </button>
-            </div>
+    <div className="hiking-post-container">
+      <HikingVote postId={postId} containerStyle={discussionContainer}/>
+      <Link to={`hikingPost/${postId}`} className="post-link">
+        <div className="hiking-post">
+          <div className="hiking-post-header">
+            <img
+              className="profile-image"
+              src="/img/profileIcon.png"
+              alt="profile"
+            />
+            <h5 className="hiking-post-username">{username}</h5>
           </div>
-        )}
-      </div>
-    </Link>
+          <h3 className="hiking-post-title">{title}</h3>
+          <div className="hiking-post-content">{content}</div>
+          {imageUrl && imageUrl.length > 0 && (
+            <div className="hiking-post-images">
+              <div className="image-slider">
+                <button
+                  className="arrow-button prev-button"
+                  onClick={handlePrevImage}
+                  disabled={currentImageIndex === 0}
+                >
+                  &lt;
+                </button>
+                <img
+                  className="hiking-post-image"
+                  src={imageUrl[currentImageIndex]}
+                  alt={`Post ${currentImageIndex + 1}`}
+                />
+                <button
+                  className="arrow-button next-button"
+                  onClick={handleNextImage}
+                  disabled={currentImageIndex === imageUrl.length - 1}
+                >
+                  &gt;
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
+      </Link>
+    </div>
   );
 };
 
--- src/components/hikingPost/HikingVote.js
+++ src/components/hikingPost/HikingVote.js
@@ -1,8 +1,54 @@
-import { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import axios from "axios";
+import styled from "styled-components";
 
-const HikingVote = ({ postId }) => {
+const VoteContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-right: 10px;
+  margin-top: 10px;
+`;
+
+const VoteCount = styled.div`
+  margin: 5px;
+  min-width: 80px;
+  font-weight: bold;
+  color: #333;
+`;
+
+const ArrowButton = styled.button`
+  border: none;
+  background: none;
+  cursor: pointer;
+  padding: 5px;
+  transition: color 0.3s;
+  margin: 5px;
+
+  &:hover {
+    color: #007bff;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  svg {
+    width: 24px;
+    height: 24px;
+  }
+
+  &.active {
+    color: #007bff;
+  }
+`;
+
+const HikingVote = ({ postId, containerStyle }) => {
   const [voteCounter, setVoteCounter] = useState(null);
+
+  const StyledVoteContainer = styled(VoteContainer)`
+    ${containerStyle}
+  `;
 
   const voteUp = () => {
     setVoteCounter((prevVoteCounter) => prevVoteCounter + 1);
@@ -47,25 +93,19 @@ const HikingVote = ({ postId }) => {
   }, [postId]);
 
   return (
-    <div className="hiking-post-vote">
-      <button className="voteUp">
-        <img
-          src="/img/voteArrowIcon.png"
-          alt="Up arrow"
-          id="up"
-          onClick={voteUp}
-        />
-      </button>
-      <div className="vote-count">{voteCounter}</div>
-      <button className="voteDown">
+    <StyledVoteContainer>
+      <ArrowButton className="voteUp" onClick={voteUp}>
+        <img src="/img/voteArrowIcon.png" alt="Up arrow" />
+      </ArrowButton>
+      <VoteCount>{voteCounter}</VoteCount>
+      <ArrowButton className="voteDown" onClick={voteDown} isRotate={true}>
         <img
           src="/img/voteArrowIcon.png"
           alt="Down arrow"
-          id="down"
-          onClick={voteDown}
+          style={{ transform: "rotate(180deg)" }}
         />
-      </button>
-    </div>
+      </ArrowButton>
+    </StyledVoteContainer>
   );
 };
 
